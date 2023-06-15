249d2a13180b6e19208b11c71bf1f548ab88d465
alexd <alex.d.doc@gmail.com>
2023-06-15 07:11:10 -0400

    pomodoro timer new and improved

--- src/PomodoroTimer/PomodoroTimer.js
+++ src/PomodoroTimer/PomodoroTimer.js
@@ -2,52 +2,78 @@ import React, { useEffect, useState } from 'react'
 import TimeDisplay from './TimeDisplay'
 
 const MIN_TO_MILLI = 60*1000
+const TIME_TO_RECESS = 6
 const audio =  new Audio('public/test.mp3')
+
+let states={
+    off: {"bg_color":"white"},
+    work: {"bg_color":"#86db8d"},
+    recess: {"bg_color":"#7b9fbd"},
+}
 
 export default function PomodoroTimer(){
     let [ worker, setWorker ] = useState(null)
     let [ time, setTime ] = useState(0)
-    let [ test, setTest ] = useState("sup")
+    let [ interval_time, setInterval_time ] = useState(0)
+    let [ change, setChange ] = useState("sup")
+    let [ state, setState ] = useState("off")
 
     useEffect(() => {
         if(!worker){
             setWorker(new Worker(new URL('./worker.js',import.meta.url)))
         }
-        if(test == 'test'){
-            setTest("sup")
+        //switches between work and recess
+        if(change == 1){
+            setChange(0)
+            audio.play()
+
+            //if == recess cause we got from recess -> work and work -> recess
+            let time_to_set = state == "recess" ? 
+                interval_time : 
+                interval_time / TIME_TO_RECESS; 
+
+            if (state == 'work') setState('recess')
+            else if (state == 'recess') setState('work')
+
             worker.postMessage({
-                time:6*MIN_TO_MILLI/60,
+                time:time_to_set,
                 paused:false
             })
         }
-    },[worker, test])
+    },[worker, change])
+
+    function stopWorker(){
+        worker.terminate()
+        setState("off")
+        setWorker(null)
+    }
 
 	function startWorker(){
+        if(state == 'work') return
+        setState("work")
 		worker.postMessage({
-			time:time,
-            paused:false
+            time:time==0?interval_time:time,
 		})
-
 		worker.onmessage = (e) =>{
             setTime(e.data)
-            if(e.data == 0){
-                setTest("test")
-            }
+            if(e.data == 0) setChange(1)
 		}
 	}
 
-    console.log(test)
     return(
         <div>
-            <div style={{background:"white", float:'left',width:"50%"}}>
+            <div style={{background:states[state].bg_color, float:'left',width:"50%"}}>
                 Intervals (m)
                 <input type="text" name="time" onChange={(e) =>{
-                    setTime(e.target.value*MIN_TO_MILLI)
+                    setInterval_time(e.target.value*MIN_TO_MILLI)
                 }}/> 
                 <br/>
                 <br/>
                 <button  onClick={() => startWorker()}> 
                     start
+                </button>
+                <button  onClick={() => stopWorker()}> 
+                    stop
                 </button>
                 <TimeDisplay time={time}/>
             </div>
--- src/PomodoroTimer/worker.js
+++ src/PomodoroTimer/worker.js
@@ -1,51 +1,21 @@
-//buffer ratio is the amount of time we run a setTimeout, the higher it is, the shorter the setTimeout
-//do not go beyond 10 as it will spawn too many timeouts
-const BUFFER_RATIO = 10
+const TIMEOUT_DURATION_RATIO = 10
 const MILLI_TO_SEC = 1000
 
-//prevents multiple timer from running
-let running = false
-
-let pauseFlag = false
-let unpauseFlag = false
-
-
 onmessage = async function(e){
-	let {time,paused} = e.data
-    pauseFlag = paused
-	if(running == false){
-        let now = new Date().getTime();
-        let target = now + time 
-		runWorker(target)
-	}
+    let now = new Date().getTime();
+    let target = now + e.data.time
+    runWorker(target)
 }
 
 async function runWorker(target){
-	running = true
     let current = new Date().getTime()
     let remainder
-
 	while(current <= target){
         await runTimer()
-        if (!pauseFlag){
-            if(unpauseFlag){
-                unpauseFlag = false
-                target = new Date().getTime() + remainder
-            }
             current = new Date().getTime()
             remainder = target - current
-            if(remainder >= 0){
-                postMessage(remainder)
-            }
-            else{
-                postMessage(0)
-            }
-		}
-        else{
-            unpauseFlag = true
-        }
-	}
-	running = false
+            remainder >= 0 ? postMessage(remainder):postMessage(0)
+    }
 }
 
 async function runTimer(){
@@ -53,7 +23,7 @@ async function runTimer(){
 	let target = now + MILLI_TO_SEC
 	while((new Date().getTime()) < target){
 		await new Promise((resolve) => {
-			setTimeout(resolve,MILLI_TO_SEC/BUFFER_RATIO)
+			setTimeout(resolve,MILLI_TO_SEC/TIMEOUT_DURATION_RATIO)
 		})
 	}
 }
